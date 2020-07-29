8275915db03c93554ee9efbc2120746520e4e128
Luciano Fernandez Oria <l_fernandez_oria@unc.edu.ar>
2020-07-29 20:00:52 -0300

    InstructionFetch updated

--- TP_FINAL/TP_BIP.srcs/sources_1/new/InstructionFetch.v
+++ TP_FINAL/TP_BIP.srcs/sources_1/new/InstructionFetch.v
@@ -6,15 +6,14 @@ module InstructionFetch(
     );
 
 wire BIP_enable, finish_program, WrRAM, RdRAM, clk, locked;
-wire[10:0] PC, Addr;
+wire[31:0] pc, Addr;
 wire[15:0] Program_Data, In_Data, Out_Data;
 wire[31:0] out_memory;
  
-ALU #(.size(32)) alu (.Op(Operation), .A(1), .B(pc), .Leds(pc));
-Data_memory #(.INIT_FILE("/home/vlad/Arquitectura2018/datos.txt")) Data_memory(.Wr(0),.Rd(1),.clk(clk),
-    .ena(1), .Addr(PC), .In_Data(In_Data), .Out_Data(out_memory));
+ALU #(.size(32)) alu (.Op(6'b100000), .A(1), .B(pc), .Leds(pc));
+Data_memory #(.INIT_FILE("~/Arquitectura2018/TP_FINAL/datos.txt")) Data_memory(.Wr(0),.Rd(1),.clk(clk),
+    .ena(1), .Addr(pc), .In_Data(In_Data), .Out_Data(out_memory));
 
 assign instruccion = out_memory;
-assign pc = PC;
 
 endmodule
