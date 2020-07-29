4382d7c3dec82552eb7935c76346675873cd6f2a
Braian Sieber <braiansieber@gmail.com>
2020-07-29 20:33:07 -0300

    Cambios realizados en instruction fetch

--- TP_FINAL/TP_BIP.srcs/sources_1/new/InstructionFetch.v
+++ TP_FINAL/TP_BIP.srcs/sources_1/new/InstructionFetch.v
@@ -2,18 +2,44 @@
 
 module InstructionFetch(
     input clk, reset,
-    output [31:0] pc, instruccion
+    output [31:0] instruccion, PC
     );
 
-wire BIP_enable, finish_program, WrRAM, RdRAM, clk, locked;
-wire[31:0] pc, Addr;
-wire[15:0] Program_Data, In_Data, Out_Data;
+wire[31:0] pc, addr;
+wire[15:0] in_data;
 wire[31:0] out_memory;
  
-ALU #(.size(32)) alu (.Op(6'b100000), .A(1), .B(pc), .Leds(pc));
-Data_memory #(.INIT_FILE("~/Arquitectura2018/TP_FINAL/datos.txt")) Data_memory(.Wr(0),.Rd(1),.clk(clk),
-    .ena(1), .Addr(pc), .In_Data(In_Data), .Out_Data(out_memory));
+// MUX + ALU(ADD)
+always @ (*)
+begin
+	if(reset)
+		addr <= 0;
+	else
+		begin
+			case (1'b0)
+				1'b0: addr <= out_alu;
+				1'b1: addr <= 0;
+			endcase
+		end
+end
+
+always @ (negedge clk, posedge reset)
+begin
+	if (reset)
+		begin
+			pc <= 32'd0;
+		end
+	else
+		begin
+			pc <= addr;
+		end
+end
+
+ALU #(.size(32)) alu (.Op(6'b100000), .A(1), .B(pc), .Leds(out_alu));
+Data_memory #(.INIT_FILE("~/Arquitectura2018/TP_FINAL/datos.txt")) Data_memory(
+    .Wr(0),.Rd(1),.clk(clk), .ena(1), .Addr(addr), .In_Data(in_data), .Out_Data(out_memory));
 
 assign instruccion = out_memory;
+assign PC = pc;
 
 endmodule
