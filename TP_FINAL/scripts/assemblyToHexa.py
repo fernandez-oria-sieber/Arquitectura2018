d35473b234a5d37ef824f1c924e3f70a60da52f6
Braian Sieber <braiansieber@gmail.com>
2021-05-25 23:37:53 -0300

    cambio en el script de python para pasar de assembler a Hexa
    Signed-off-by: Braian Sieber <braiansieber@gmail.com>

--- TP_FINAL/scripts/assemblyToHexa.py
+++ TP_FINAL/scripts/assemblyToHexa.py
@@ -1,83 +1,90 @@
-
 import os
 import re
 
-type_R_instructions = "SLL,SRL,SRA,SLLV,SRLV,SRAV,ADDU,SUBU,AND,OR,XOR,NOR,SLT"
+instruction_format = {
+    # R-type
+    "SLL": "000000_00000_{l2}_{l1}_{l3}_000000",  # SLL rd, rt, sa
+    "SRL": "000000_00000_{l2}_{l1}_{l3}_000010",
+    "SRA": "000000_00000_{l2}_{l1}_{l3}_000011",
+    "SLLV": "000000_00000_{l3}_{l2}_{l1}_000100",  # SLLV rd, rt ,rs
+    "SRLV": "000000_00000_{l3}_{l2}_{l1}_000110",
+    "SRAV": "000000_00000_{l3}_{l2}_{l1}_000111",
+    "ADDU": "000000_00000_{l2}_{l3}_{l1}_100001",  # ADDU rd, rs, rt
+    "SUBU": "000000_00000_{l2}_{l3}_{l1}_100011",
+    "AND": "000000_00000_{l2}_{l3}_{l1}_100100",
+    "OR": "000000_00000_{l2}_{l3}_{l1}_100101",
+    "XOR": "000000_00000_{l2}_{l3}_{l1}_100110",
+    "NOR": "000000_00000_{l2}_{l3}_{l1}_100111",
+    "SLT": "000000_00000_{l2}_{l3}_{l1}_101010",
+    # I-Type
+    "LB": "100000_{l3}_{l1}_{l2}",  # LB rt, offset(base)
+    "LH": "100001_{l3}_{l1}_{l2}",
+    "LW": "100011_{l3}_{l1}_{l2}",
+    "LWU": "100111_{l3}_{l1}_{l2}",
+    "LBU": "100100_{l3}_{l1}_{l2}",
+    "LHU": "100001_{l3}_{l1}_{l2}",
+    "SB": "101000_{l3}_{l1}_{l2}",
+    "SH": "101001_{l3}_{l1}_{l2}",
+    "SW": "101011_{l3}_{l1}_{l2}",
+    "ADDI": "001000_{l2}_{l1}_{l3}",  # ADDI rt, rs, inmediate
+    "ANDI": "001100_{l2}_{l1}_{l3}",
+    "ORI": "001101_{l2}_{l1}_{l3}",
+    "XORI": "001110_{l2}_{l1}_{l3}",
+    "LUI": "001111_00000_{l1}_{l2}",  # LUI rt, inmediate
+    "SLTI": "001010_{l2}_{l1}_{l3}",  # SLTI rt, rs, inmediate
+    "BEQ": "000100_{l1}_{l2}_{l3}",  # ADDI rs, rt, offset
+    "BNE": "000101_{l1}_{l2}_{l3}",
+    "J": "000010_{l1}",  # J instruction_index
+    "JAL": "000011_{l1}",
+    # J-Type
+    "JR": "000000_{l1}_00000_00000_00000_001000",  # JR rs
+    "JALR": "000000_{l2}_00000_{l1}_00000_001001",  # JALR rd, rs
+}
 
 
 def format_value(value):
     return "{0:05b}".format(int(value))
 
+
+def format_list(l):
+    return [value if i == 0 else format_value(value) for i, value in enumerate(l)]
+
+
 def format(l):
-    instruction_format = {
-        # R-type
-        "SLL"   : f'000000_00000_{format_value(l[2])}_{format_value(l[1])}_{format_value(l[3])}_000000',    # SLL rd, rt, sa
-        "SRL"   : f'000000_00000_{format_value(l[2])}_{format_value(l[1])}_{format_value(l[3])}_000010',  
-        "SRA"   : f'000000_00000_{format_value(l[2])}_{format_value(l[1])}_{format_value(l[3])}_000011',
-        "SLLV"  : f'000000_00000_{format_value(l[3])}_{format_value(l[2])}_{format_value(l[1])}_000100',    # SLLV rd, rt ,rs
-        "SRLV"  : f'000000_00000_{format_value(l[3])}_{format_value(l[2])}_{format_value(l[1])}_000110',
-        "SRAV"  : f'000000_00000_{format_value(l[3])}_{format_value(l[2])}_{format_value(l[1])}_000111',
-        "ADDU"  : f'000000_00000_{format_value(l[2])}_{format_value(l[3])}_{format_value(l[1])}_100001',    # ADDU rd, rs, rt
-        "SUBU"  : f'000000_00000_{format_value(l[2])}_{format_value(l[3])}_{format_value(l[1])}_100011',
-        "AND"   : f'000000_00000_{format_value(l[2])}_{format_value(l[3])}_{format_value(l[1])}_100100',
-        "OR"    : f'000000_00000_{format_value(l[2])}_{format_value(l[3])}_{format_value(l[1])}_100101',
-        "XOR"   : f'000000_00000_{format_value(l[2])}_{format_value(l[3])}_{format_value(l[1])}_100110',
-        "NOR"   : f'000000_00000_{format_value(l[2])}_{format_value(l[3])}_{format_value(l[1])}_100111',
-        "SLT"   : f'000000_00000_{format_value(l[2])}_{format_value(l[3])}_{format_value(l[1])}_101010',
+    return (
+        instruction_format.get(l[0])
+        .replace("{l1}", l[1])
+        .replace("{l2}", l[2])
+        .replace("{l3}", l[3])
+    )
 
-        # I-Type
-        "LB"    : f'100000_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',    # LB rt, offset(base)
-        "LH"    : f'100001_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',
-        "LW"    : f'100011_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',
-        "LWU"   : f'100111_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',
-        "LBU"   : f'100100_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',
-        "LHU"   : f'100001_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',
-        "SB"    : f'101000_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',
-        "SH"    : f'101001_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',
-        "SW"    : f'101011_{format_value(l[3])}_{format_value(l[1])}_{format_value(l[2])}',
-        "ADDI"  : f'001000_{format_value(l[2])}_{format_value(l[1])}_{format_value(l[3])}',   # ADDI rt, rs, inmediate
-        "ANDI"  : f'001100_{format_value(l[2])}_{format_value(l[1])}_{format_value(l[3])}',
-        "ORI"   : f'001101_{format_value(l[2])}_{format_value(l[1])}_{format_value(l[3])}',
-        "XORI"  : f'001110_{format_value(l[2])}_{format_value(l[1])}_{format_value(l[3])}',
-        "LUI"   : f'001111_00000_{format_value(l[1])}_{format_value(l[2])}', # LUI rt, inmediate
-        "SLTI"  : f'001010_{format_value(l[2])}_{format_value(l[1])}_{format_value(l[3])}',   # SLTI rt, rs, inmediate
-        "BEQ"   : f'000100_{format_value(l[1])}_{format_value(l[2])}_{format_value(l[3])}',      # ADDI rs, rt, offset
-        "BNE"   : f'000101_{format_value(l[1])}_{format_value(l[2])}_{format_value(l[3])}',
-        "J"     : f'000010_{format_value(l[1])}',   # J instruction_index
-        "JAL"   : f'000011_{format_value(l[1])}',
-
-        # J-Type
-        "JR"    : f'000000_{format_value(l[1])}_00000_00000_00000_001000',  # JR rs
-        "JALR"  : f'000000_{format_value(l[2])}_00000_{format_value(l[1])}_00000_001001'  # JALR rd, rs
-
-
-    }
-
-    return instruction_format[l[0]]
 
 def parser(instruction):
     instruction_list = [value.strip(",\n)") for value in re.split(" |\(", instruction)]
-    while(len(instruction_list)<4):
+    while len(instruction_list) < 4:
         instruction_list.append("0")
     return instruction_list
 
+
 def converter(instruction):
-    instruction_list = parser(instruction)
-    value = int(format(instruction_list),2)
-    hex_value = '0x{0:08X}'.format(value)
+    instruction_list = format_list(parser(instruction))
+    value = int(format(instruction_list), 2)
+    hex_value = "0x{0:08X}".format(value)
     return hex_value
+
 
 def main():
     hexa_file = open("instructionsHexa.txt", "w")
     if os.path.isfile("instructionsAssembly.txt"):
         assembly_file = open("instructionsAssembly.txt")
         for instruction in assembly_file:
-            hexa_file.write(converter(instruction)+"\n")
+            hexa_file.write(converter(instruction) + "\n")
         hexa_file.close()
         assembly_file.close()
         print("Conversion finalizada.")
     else:
         print("File doasn't exist.")
+
 
 if __name__ == "__main__":
     main()
