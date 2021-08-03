a412c8217e00ee863a7254469f8304794d6aa6d9
Braian Sieber <braiansieber@gmail.com>
2021-08-03 12:38:42 -0300

    add:TP_FINAL/TP_FINAL.srcs/sources_1/new/InstructionFetch.v: comentarios en los parametros
    Signed-off-by: Braian Sieber <braiansieber@gmail.com>

--- TP_FINAL/TP_FINAL.srcs/sources_1/new/InstructionFetch.v
+++ TP_FINAL/TP_FINAL.srcs/sources_1/new/InstructionFetch.v
@@ -3,22 +3,22 @@
 module InstructionFetch #(parameter INSTRUCTION_FILE = "")
                         (input clk,
                          input reset,
-                         input isPCSel,                // Selector del MUX que viene de una AND en la etapa MEM, entre Branch y Zero
-                         input isPCWrite,              // Selector que viene desde el Hazard Unit
-                         input write_enable,
-                         input [10:0] IMEM_addr,
-                         input [31:0] inPCJump,        // Salida del sumador en la etapa EX
-                         input [31:0] inInstruction,
-                         output outFinish,
-                         output [31:0] out_clk_counter,
-                         output [31:0] outInstruction, //
+                         input isPCSel,                 // Selector del MUX que viene de una AND en la etapa MEM, entre Branch y Zero
+                         input isPCWrite,               // Selector que viene desde el Hazard Unit
+                         input write_enable,            // UART
+                         input [10:0] IMEM_addr,        // UART -> Dirección donde se va a escribir el dato
+                         input [31:0] inPCJump,         // Salida del sumador en la etapa EX
+                         input [31:0] inInstruction,    // UART
+                         output outFinish,              // bit para finalizar el MIPS
+                         output [31:0] out_clk_counter, // Cantidad de ciclos de clock desde el inicio
+                         output [31:0] outInstruction,
                          output [31:0] outPC);
     
     // Registros
     reg finish;
     reg [10:0] memory_address;
-    reg [31:0] pc, addr, clk_counter; // dirección de acceso a la memoria, asociada al pc
-    wire [319:0] values;
+    reg [31:0] pc, addr, clk_counter;   // dirección de acceso a la memoria, asociada al pc
+    wire [319:0] values;                // no interesan estos valores
     
     DataMemory #(.INIT_FILE(INSTRUCTION_FILE)) instruction_memory(
     .clk(clk),
@@ -46,7 +46,7 @@ module InstructionFetch #(parameter INSTRUCTION_FILE = "")
             if (isPCWrite) 
             begin
                 pc <= (isPCSel) ? inPCJump : pc + 1;
-                clk_counter <= clk_counter + 1'b1;
+                clk_counter <= clk_counter + 1'b1;  // TODO: esto debería sumar siempre que funcione el MIPS
             end
             if (outInstruction == 32'b0) finish <= 1'b1;
         end
