7cd3af0fe63465d86bfaeb4dcdf3c2626bd11611
Luciano Fernandez Oria <l_fernandez_oria@unc.edu.ar>
2021-08-17 20:52:49 -0300

    MIPS testbench added.

---
+++ TP_FINAL/TP_BIP.srcs/sim_1/new/MIPS_Testbench.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/28/2021 07:49:43 PM
+// Design Name: 
+// Module Name: MIPS_Testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MIPS_Testbench(
+
+    );
+    
+    reg [31:0] instruction;
+    reg [10:0] IMEM_addr;
+    wire [1813:0] values;
+    reg wr_enable, start, clk, rst; 
+    wire finish;
+    
+    MIPS #(.INSTRUCTION_FILE("/home/vlad/Documents/Arquitectura2018/TP_FINAL/scripts/instructionsHexa.txt"),
+           .DATA_FILE("/home/vlad/Documents/Arquitectura2018/TP_FINAL/scripts/datos.txt")) 
+            int_mips(clk, rst, wr_enable, start, IMEM_addr, instruction, finish, values);
+     
+    initial
+    begin
+    clk         =   1'b0;
+    rst         =   1'b1;
+    wr_enable   =   1'b0;
+    start       =   1'b1;
+    IMEM_addr   =   11'b0;
+    instruction =   32'b0;
+    #10 
+    rst         =   1'b0;
+    end
+    
+    always @(posedge clk)
+    begin
+        if(finish)
+        begin
+            $display("TERMINOOOOO!!!!!!!");
+            $display("VALUES: %b", values);
+            start = 1'b0;
+        end
+    end
+    
+    
+    
+    always #5 clk=~clk;  // Genero los ciclos de reloj
+
+endmodule
